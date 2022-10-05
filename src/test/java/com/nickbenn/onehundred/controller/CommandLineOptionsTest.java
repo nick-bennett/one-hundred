b1e45a1016803c7ef9983b59db556bb1720edc03
nick-bennett <nick@nickbenn.com>
2022-10-05 17:57:25 -0600

    Add tests stubs.

---
+++ src/test/java/com/nickbenn/onehundred/controller/CommandLineOptionsTest.java
@@ -0,0 +1,15 @@
+package com.nickbenn.onehundred.controller;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+// TODO: 2022-10-05 Replace test stubs.
+class CommandLineOptionsTest {
+
+  @Test
+  void parse() {
+    fail("Test not implemented");
+  }
+
+}
---
+++ src/test/java/com/nickbenn/onehundred/model/GameTest.java
@@ -0,0 +1,56 @@
+package com.nickbenn.onehundred.model;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+// TODO: 2022-10-05 Replace test stubs.
+
+class GameTest {
+
+  @Test
+  void play() {
+    fail("Test not implemented");
+  }
+
+  @Test
+  void getOperation() {
+    fail("Test not implemented");
+  }
+
+  @Test
+  void getUpperBound() {
+    fail("Test not implemented");
+  }
+
+  @Test
+  void getMaxMove() {
+    fail("Test not implemented");
+  }
+
+  @Test
+  void getTarget() {
+    fail("Test not implemented");
+  }
+
+  @Test
+  void getCurrentCount() {
+    fail("Test not implemented");
+  }
+
+  @Test
+  void getState() {
+    fail("Test not implemented");
+  }
+
+  @Test
+  void isFirstMove() {
+    fail("Test not implemented");
+  }
+
+  @Test
+  void getRemaining() {
+    fail("Test not implemented");
+  }
+
+}
